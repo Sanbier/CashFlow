b803c5637b236f2d5fc75ad783d17d1cb4c0e106
Sanbier <89169066+Sanbier@users.noreply.github.com>
2026-01-06 01:06:49 +0700

    feat: Add 'url' package and fix __dirname in vite config
    Introduces the 'url' package to support ESM module resolution. Also, defines `__dirname` in `vite.config.ts` to ensure compatibility with Node.js ES modules, as it is not available by default.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
+
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+
+// Fix: Define __dirname for ES modules compatibility as it is not available in Node.js ES modules by default
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
